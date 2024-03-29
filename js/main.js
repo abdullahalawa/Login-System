fb9155ede69a706012c72873c6491e7753d3a47c
Abdullah Alawa <awamah.group@gmail.com>
2024-03-29 06:35:09 +0300

    finish signup with validations

--- js/main.js
+++ js/main.js
@@ -16,8 +16,38 @@ var mailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
 // Users Repository
 var savedUsers = [];
 
+// get Data from localStorage when run the
+// app for the first time on browser
 if (localStorage.getItem("users") != null) {
   savedUsers = JSON.parse(localStorage.getItem("users"));
+}
+
+// email validation
+function isValidMail(element, regex) {
+  if (regex.test(element.value) != true) {
+    return false;
+  }
+  return true;
+}
+
+// check if inputs are empty
+function isInputsEmpty(obj) {
+  for (var key in obj) {
+    if (obj[key] === "") {
+      return true;
+    }
+    return false;
+  }
+}
+
+// search for User in savedUsers
+// return result in object if exists
+// or return undefind if not
+function isUserExisted(newAddedUserEmail) {
+  if (savedUsers.find((user) => user.email === newAddedUserEmail)) {
+    return true;
+  }
+  return false;
 }
 
 // New users registration
@@ -32,93 +62,54 @@ function signupNewUsers() {
     password: signupPassword,
   };
 
-  if (
-    savedUsers.length == 0 &&
-    isValidMail(signupEmailInput, mailRegex) == true
-  ) {
+  if (isInputsEmpty(newSignedUsers) == true) {
+    document
+      .querySelector("#emptyError")
+      .classList.replace("d-none", "d-block");
+    return false;
+  } else if (isInputsEmpty(newSignedUsers) == false) {
+    document
+      .querySelector("#emptyError")
+      .classList.replace("d-block", "d-none");
+  }
+
+  if (isValidMail(signupEmailInput, mailRegex) == false) {
+    document
+      .querySelector("#mailValidationMsg")
+      .classList.replace("d-none", "d-block");
+    return false;
+  } else if (isValidMail(signupEmailInput, mailRegex) == true) {
+    document
+      .querySelector("#mailValidationMsg")
+      .classList.replace("d-block", "d-none");
+  }
+
+  if (savedUsers.length == 0) {
     savedUsers.push(newSignedUsers);
     localStorage.setItem("users", JSON.stringify(savedUsers));
-  } else {
-    if (
-      isUserExist(newSignedUsers.email) == false &&
-      isValidMail(signupEmailInput, mailRegex) == true
-    ) {
-      savedUsers.push(newSignedUsers);
-      console.log(savedUsers);
-      localStorage.setItem("users", JSON.stringify(savedUsers));
-      window.location.href = "../index.html";
-    } else {
-      console.log("Error exists");
-    }
-  }
-}
+    window.location.href = "../index.html";
 
-// search for User in savedUsers
-// return result in object if exists
-// or return undefind if not
-function isUserExist(newAddedUserEmail) {
-  if (savedUsers.find((user) => user.email === newAddedUserEmail)) {
-    signupPasswordInput.nextElementSibling.children[1].classList.replace(
-      "d-none",
-      "d-block"
-    );
     return true;
   }
 
-  return false;
-}
-
-// email validation
-function isValidMail(element, regex) {
-  if (regex.test(element.value) != true) {
-    element.nextElementSibling
-      .querySelector("p")
+  if (isUserExisted(newSignedUsers.email) == true) {
+    document
+      .querySelector("#emailError")
       .classList.replace("d-none", "d-block");
-    return false;
-  }
-
-  element.nextElementSibling
-    .querySelector("p")
-    .classList.replace("d-block", "d-none");
-  return true;
-}
-
-// login
-function userLogin(userMail, userPassword) {
-  if (savedUsers.find((user) => user.email === userMail)) {
-    window.location.href = "../home.html";
   } else {
+    savedUsers.push(newSignedUsers);
+    localStorage.setItem("users", JSON.stringify(savedUsers));
+    window.location.href = "../index.html";
   }
 }
 
 // EventListeners on the app
-if (window.location.href === "../signup.html") {
-  // signup
-  signupButton.addEventListener("click", function (e) {
-    e.preventDefault();
-    signupNewUsers();
-  });
+// signup
+signupButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  signupNewUsers();
+});
 
-  signupButton.addEventListener("click", function (e) {
-    isValidMail(signupEmailInput, mailRegex);
-  });
-}
-
-// login
-
-if (window.location.href === "../index.html") {
-}
-
-// loginBtn.addEventListener("click", function (e) {});
-
-function isLoginUserValid(loginEmail, loginPassword) {
-  var results = savedUsers.some(
-    (user) => user.email === loginEmail && user.password === loginPassword
-  );
-
-  if (results == true) {
-    return true;
-  } else {
-    return false;
-  }
-}
+signupButton.addEventListener("click", function (e) {
+  isValidMail(signupEmailInput, mailRegex);
+});
